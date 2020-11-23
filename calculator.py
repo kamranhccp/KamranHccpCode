73c65b8c34d5402c1d8f4afe8c0bcd2bcefa27c9
Kamran Mansoor <50527133+kamranhccp@users.noreply.github.com>
2020-11-23 20:23:04 +0500

    Add files via upload
    This is code using Python-"Calculator Code made by @kamranhccp"
    This will show you How Python Calculator is made?
    Thanks.

---
+++ calculator.py
@@ -0,0 +1,15 @@
+num1 = eval(input("Enter your First Number: "))
+num2 = eval(input("Enter your Second Number: "))
+oper = input("Enter your Opertion(+,-,*,/,%): ")
+if oper == "+":
+    print("Your addition is ", (num1 + num2), ".")
+elif oper == "-":
+    print("Your Subtraction is ", (num1 - num2), ".")
+elif oper == "*":
+    print("Your Multiplication is ", (num1 * num2), ".")
+elif oper == "/":
+    print("Your Division is ", (num1 / num2), ".")
+elif oper == "%":
+    print("Your Reminder is ", (num1 % num2), ".")
+else:
+    print("Invalid Input!")
